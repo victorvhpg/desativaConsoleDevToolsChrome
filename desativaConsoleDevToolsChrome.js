66a784be697e75ea26434e2e9e33d346265121b5
Victor Hugo <victorvhpg@gmail.com>
2014-03-21 00:48:08 -0300

    exibindo webcam dentro do console dev tools kkkkkk

--- desativaConsoleDevToolsChrome.js
+++ desativaConsoleDevToolsChrome.js
@@ -17,7 +17,10 @@ https://github.com/victorvhpg/desativaConsoleDevToolsChrome
 (function(window) {
 
     var _init = false;
+    var _aguardandoPermissao = false;
     var _config = {
+        webcam: false,
+        videoSrc: "",
         css: "color:#000;background-color:yellow;font-weight:bolder;font-size:90px",
         msg: "-- DESATIVADO :( "
     };
@@ -25,9 +28,12 @@ https://github.com/victorvhpg/desativaConsoleDevToolsChrome
     var desativaConsoleDevToolsChrome = {
         config: _config,
         timer: 0,
+        srcWebCam: "",
+        canvas: null,
+        ctx: null,
         pisca: function(msg) {
             var that = this;
-           window.clearInterval(this.timer);
+            window.clearInterval(this.timer);
             var cont = 0;
             this.timer = window.setInterval(function() {
                 if ((++cont % 2) == 0) {
@@ -38,9 +44,64 @@ https://github.com/victorvhpg/desativaConsoleDevToolsChrome
             }, 300);
         },
 
+        exibeVideo: function(src) {
+            var largura, altura, video, that;
+            that = this;
+            largura = 600;
+            altura = 300;
+            if (!this.ctx) {
+                this.canvas = document.createElement("canvas");
+                this.ctx = this.canvas.getContext("2d");
+                this.canvas.width = largura;
+                this.canvas.height = altura;
+            }
+            window.clearInterval(this.timer);
+            video = document.createElement("video");
+            video.width = largura;
+            video.height = altura;
+            video.src = src;
+            video.onloadedmetadata = function(e) {
+                video.play();
+                var limpa = 0;
+                that.timer = window.setInterval(function() {
+                    if (++limpa % 30 == 0) {
+                        that.injectedScriptHost.clearConsoleMessages();
+                    }
+                    that.ctx.drawImage(video, 0, 0, largura, altura);
+                    var src = that.canvas.toDataURL("image/png");
+                    var log = ["%c", "font-size:1px;padding:" + (altura / 2) + "px " + (largura / 2) + "px;background: url(" + src + ");line-height:" + altura + "px; color: transparent;"];
+                    window.setTimeout(Function.prototype.apply.bind(console.log, console, log), 1);
+                },400);
+            };
+        },
+
+        getWebCam: function(fnOk, fnErro) {
+            var that = this;
+            if (_aguardandoPermissao) {
+                return;
+            }
+            if (this.srcWebCam) {
+                fnOk.apply(this, [this.srcWebCam]);
+                return;
+            }
+            _aguardandoPermissao = true;
+            window.navigator.webkitGetUserMedia({
+                video: true
+            }, function(localMediaStream) {
+                _aguardandoPermissao = false;
+                that.srcWebCam = window.URL.createObjectURL(localMediaStream);
+                fnOk.apply(that, [that.srcWebCam]);
+            }, function(err) {
+                _aguardandoPermissao = false;
+                that.srcWebCam = "";
+                alert("Erro ao obter webcam");
+                fnErro.apply(that);
+            });
+        },
+
         desativa: function() {
             var that = this;
-            var msg = ["%c" + this.config.msg,this.config.css];
+            var msg = ["%c" + this.config.msg, this.config.css];
             Object.defineProperty(this.injectedScript, "_inspect", {
                 get: function() {
                     return window.alert.bind(window, that.config.msg);
@@ -49,8 +110,19 @@ https://github.com/victorvhpg/desativaConsoleDevToolsChrome
             Object.defineProperty(this.injectedScriptHost, "evaluate", {
                 get: function() {
                     return function() {
-                        that.pisca(msg);
-                        return "";
+
+                        if (that.config.webcam) {
+                            that.getWebCam(function(src) {
+                                that.exibeVideo(src);
+                            }, function() {
+                                that.pisca(msg);
+                            });
+                        } else if (that.config.videoSrc) {
+                            that.exibeVideo(that.config.videoSrc);
+                        } else {
+                            that.pisca(msg);
+                        }
+                        return (that.config.webcam && _aguardandoPermissao) ? "De permissao na webcam!!" : "";
                     };
                 }
             });
@@ -74,6 +146,8 @@ https://github.com/victorvhpg/desativaConsoleDevToolsChrome
             var that = this;
             this.config.css = config.css || _config.css;
             this.config.msg = config.msg || _config.msg;
+            this.config.webcam = config.webcam || _config.webcam;
+            this.config.videoSrc = config.videoSrc || _config.videoSrc;
             //para versao > 33 do chrome 
             // por enquanto fazendo gambiarra sobreescrevendo metodo nativo .call :(
             //testado ate  versao 35.0.1899.0 canary
